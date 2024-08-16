f3c8fcd2f1e078287717bfc902a18167ba3a8df2
vishad-p-gortnm <vishad.p@gortnm.in>
2024-08-16 21:38:47 +0530

    Build simple program who generate random person name who pay bill for today party

--- Day 4/list_demo.py
+++ Day 4/list_demo.py
@@ -1,5 +1,10 @@
 import random as rd
 friends = ["Alice","Bob","Charlie","David","Emanuel"]
+#Use this simple approach to generate random number between 0 to N.
 who_pay_bill = friends[rd.randint(0,4)]
 print(who_pay_bill)
 print(f"Today bill is paid by {who_pay_bill}")
+
+#alternate approach to get random item from the sequence data type
+who_pay_bill1 = rd.choice(friends)
+print("Alternate person who will pay bill is ", who_pay_bill1)
